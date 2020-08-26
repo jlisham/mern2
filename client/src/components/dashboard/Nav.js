88e9bf00733feb8c0aa2bebb4c15da631ceecc38
jlisham <jlisham@hotmail.com>
2020-08-26 16:11:44 -0500

    update through edit/add profile with goBack error

---
+++ client/src/components/dashboard/Nav.js
@@ -0,0 +1,29 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+const Nav = () => {
+  return (
+    <div className="dash-buttons">
+      <Link to="/manage-profile" className="btn btn-light">
+        <span>
+          <i className="fas fa-user-circle text-primary"></i>
+        </span>
+        Edit Profile
+      </Link>
+      <Link to="/add-experience" className="btn btn-light">
+        <span>
+          <i className="fab fa-black-tie text-primary"></i>
+        </span>
+        Add Experience
+      </Link>
+      <Link to="/add-education" className="btn btn-light">
+        <span>
+          <i className="fas fa-graduation-cap text-primary"></i>
+        </span>
+        Add Education
+      </Link>
+    </div>
+  );
+};
+
+export default Nav;
