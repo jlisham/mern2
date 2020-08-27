e161b10d54549a34eace35459be43aa2a2ac2f05
jlisham <jlisham@hotmail.com>
2020-08-27 09:07:18 -0500

    through creation and deletion of experience and profile/account

---
+++ client/src/components/profile-form/ManageEd.js
@@ -0,0 +1,122 @@
+import React, { Fragment, useState } from "react";
+import { Link, withRouter } from "react-router-dom";
+import PropTypes from "prop-types";
+import { connect } from "react-redux";
+import { manageEd } from "../../actions/profile";
+
+const ManageEd = ({ manageEd, history }) => {
+  const [formData, setFormData] = useState({
+    school: "",
+    degree: "",
+    field: "",
+    from: "",
+    to: "",
+    current: false,
+    description: "",
+  });
+
+  const [toDateActive, toggleToDate] = useState(false);
+  const { school, degree, field, from, to, current, description } = formData;
+
+  const onChange = (e) =>
+    setFormData({ ...formData, [e.target.name]: e.target.value });
+
+  return (
+    <Fragment>
+      <h1 className="large text-primary">Add a School</h1>
+      <p className="lead">
+        <i className="fas fa-code-branch"></i> Add Schools/Educational
+        Experience
+      </p>
+      <small>* = required field</small>
+      <form
+        className="form"
+        onSubmit={(e) => {
+          e.preventDefault();
+          manageEd(formData, history);
+        }}
+      >
+        <div className="form-group">
+          <input
+            type="text"
+            placeholder="* School"
+            name="school"
+            value={school}
+            onChange={(e) => onChange(e)}
+            required
+          />
+        </div>
+        <div className="form-group">
+          <input
+            type="text"
+            placeholder="* Degree"
+            name="degree"
+            value={degree}
+            onChange={(e) => onChange(e)}
+            required
+          />
+        </div>
+        <div className="form-group">
+          <input
+            type="text"
+            placeholder="Field of Study"
+            name="field"
+            value={field}
+            onChange={(e) => onChange(e)}
+          />
+        </div>
+        <div className="form-group">
+          <h4>From Date</h4>
+          <input
+            type="date"
+            name="from"
+            value={from}
+            onChange={(e) => onChange(e)}
+          />
+        </div>
+        <div className="form-group">
+          <p>
+            <input
+              type="checkbox"
+              name="current"
+              value={current}
+              onChange={(e) => {
+                setFormData({ ...formData, current: current });
+                toggleToDate(!toDateActive);
+              }}
+            />{" "}
+            Graduated
+          </p>
+        </div>
+        <div className="form-group">
+          <h4>To Date</h4>
+          <input
+            type="date"
+            name="to"
+            value={to}
+            onChange={(e) => onChange(e)}
+            disabled={toDateActive ? "disabled" : ""}
+          />
+        </div>
+        <div className="form-group">
+          <textarea
+            name="description"
+            cols="30"
+            rows="5"
+            placeholder="Job Description"
+            value={description}
+            onChange={(e) => onChange(e)}
+          ></textarea>
+        </div>
+        <input type="submit" className="btn btn-primary my-1" />
+        <a className="btn btn-light my-1" href="dashboard.html">
+          Go Back
+        </a>
+      </form>
+    </Fragment>
+  );
+};
+
+ManageEd.propTypes = { manageEd: PropTypes.func.isRequired };
+
+export default connect(null, { manageEd })(ManageEd);
